d70f0d0e6b71e76071fc55352f137c4720d3841d
kelson9 <kelsonngoe@gmail.com>
2023-11-03 11:09:46 +0100

    Added lambda functions in serviceImpl and slf4j with other updates based on code review

--- category-service/src/main/java/com/edmart/category/serviceImpl/CategoryServiceImpl.java
+++ category-service/src/main/java/com/edmart/category/serviceImpl/CategoryServiceImpl.java
@@ -3,59 +3,68 @@ package com.edmart.category.serviceImpl;
 import com.edmart.category.Mappers.CategoryMapper;
 import com.edmart.category.dto.CategoryDTO;
 import com.edmart.category.entity.Category;
+import com.edmart.category.exception.CategoryNotFoundException;
 import com.edmart.category.repository.CategoryRepository;
 import com.edmart.category.service.CategoryService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
 
-    private CategoryRepository categoryRepository;
-
-    public CategoryServiceImpl(CategoryRepository categoryRepository){
-        this.categoryRepository=categoryRepository;
-    }
+    private final CategoryRepository categoryRepository;
 
     @Override
     public void createCategory(CategoryDTO categoryDTO) {
-        categoryRepository.save(CategoryMapper.INSTANCE.mapToCategory(categoryDTO));
+        try {
+            categoryRepository.save(CategoryMapper.INSTANCE.mapToCategory(categoryDTO));
+
+        }catch (Exception e){
+            log.error("Error creating category with name {} caused by {}",categoryDTO.categoryName(),e.getCause());
+        }
     }
 
     @Override
     public List<CategoryDTO> getAllCategories() {
-        return CategoryMapper.INSTANCE.mapToCategoryDTOs(categoryRepository.findAll());
-    }
+        return categoryRepository.findAll().stream()
+                .map(CategoryMapper.INSTANCE::mapToCategoryDTO)
+                .collect(Collectors.toList());    }
 
     @Override
     public CategoryDTO getCategory(Long categoryId) {
-        Optional<Category> categoryOptional=categoryRepository.findById(categoryId);
-        if(!categoryOptional.isPresent()){
-            return null;
-        }
-        Category category=categoryOptional.get();
-        return CategoryMapper.INSTANCE.mapToCategoryDTO(category);
+        return categoryRepository.findById(categoryId)
+                .map(category -> CategoryMapper.INSTANCE.mapToCategoryDTO(category))
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
     }
 
     @Override
     public void updateCategory(Long categoryId, CategoryDTO categoryDTO) {
-        Optional<Category> categoryOptional = categoryRepository.findById(categoryId);
-        if (!categoryOptional.isPresent()) {
-            return;
-        }
-        Category category = categoryOptional.get();
+        try {
+            Optional<Category> categoryOptional = categoryRepository.findById(categoryId);
+            if (!categoryOptional.isPresent()) {
+                throw new CategoryNotFoundException("Category not found");
+            }
+            Category category = categoryOptional.get();
 
-        CategoryMapper.INSTANCE.mapUpdateToCategory(categoryDTO, category);
-        categoryRepository.save(category);
+            CategoryMapper.INSTANCE.mapUpdateToCategory(categoryDTO, category);
+            categoryRepository.save(category);
+        }catch (Exception e){
+            log.error("Error updating category with Id {} caused by {}",categoryId,e.getCause());
+        }
     }
 
     @Override
     public void deleteCategory(Long categoryId) {
         Optional<Category> categoryOptional=categoryRepository.findById(categoryId);
         if(!categoryOptional.isPresent()){
-            return;
+            throw new CategoryNotFoundException("Category not found");
         }
         categoryRepository.deleteById(categoryId);
     }
