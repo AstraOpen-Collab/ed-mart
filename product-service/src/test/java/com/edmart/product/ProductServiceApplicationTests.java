d6588e894ae38862a4524b1dc89bb96c400bdb33
DanielGraham123 <dg.danielboaz@yahoo.com>
2023-11-17 22:55:57 +0100

    initiated product service

---
+++ product-service/src/test/java/com/edmart/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmart.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
